c3f222ef655237683df12e654115af37d222adf0
AngelMukiiza <angemwiza100@gmail.com>
2025-02-18 11:55:34 +0300

    tasksDb🍕
    i finished tasks Database design

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from users.models import CustomUser
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    assigned_to = models.ForeignKey(CustomUser,on_delete=models) 
